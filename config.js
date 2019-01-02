5e64380febff99b9cb023b930af912974d4162ce
Lakshminarayanan B <lakshminarayananb@Lakshmi.local>
2019-01-02 15:45:59 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+module.exports = {
+    consumer_key: 'jTKDx3Mw1iHCfXYaSTp1TQKnJ',  
+    consumer_secret: 'YV0eLQOQiGn50CpCJJPC2vDpnynEUKKSQOtvJ4L0wDeWLxryBT',
+    access_token: '162783192-VAEacBvOUDWqUP2berIWf4Jwuc16aThs6d6Eiocc',  
+    access_token_secret: 'nYDNOphVNppGNHK0WmUXvnGAdonwXu562gV85RICQ63Ca'
+  }
